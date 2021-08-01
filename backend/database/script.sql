b690edaccc4333a7b8d19e9424d6c729dff0ae8e
Ubuntu <ubuntu@ip-172-31-31-6.us-west-2.compute.internal>
2021-08-01 18:00:34 +0000

    Mysql setup

---
+++ backend/database/script.sql
@@ -0,0 +1,10 @@
+create database project;
+use project;
+CREATE TABLE IF NOT EXISTS `project`.`Person` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(250) NOT NULL,
+  `email` VARCHAR(250) NOT NULL,
+  `password` VARCHAR(250) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB;
