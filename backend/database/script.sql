54c9c3605024fa0d24acb4a30edcbb6a53f84030
Miguel Esteban <3003686820101@ingenieria.usac.edu.gt>
2021-08-02 12:49:42 -0600

    [FEAT]: sql route

--- backend/database/script.sql
+++ backend/database/script.sql
@@ -8,3 +8,5 @@ CREATE TABLE IF NOT EXISTS `project`.`Person` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
 ENGINE = InnoDB;
+
+select * from Person;
--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,41 @@
 from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
 from flask_cors import CORS
+import hashlib
+
+'''import sys
+sys.path.append('/run/secrets/gueban-secret')
+import secret'''
+
+import secret
 
 server = Flask(__name__)
 CORS(server)
 
+server.config['MYSQL_HOST'] = secret.keys['Mysql']['HOST']
+server.config['MYSQL_PORT'] = secret.keys['Mysql']['PORT']
+server.config['MYSQL_USER'] = secret.keys['Mysql']['USER']
+server.config['MYSQL_PASSWORD'] = secret.keys['Mysql']['PASSWORD']
+server.config['MYSQL_DB'] = secret.keys['Mysql']['DB']
+
+mysql = MySQL(server)
+
+#----------------HEALTH CHECK------------
 @server.route('/', methods=["GET"])
 def ping():
     return jsonify({'message' : "Hi! I'm listening Opus by Eric Prydz and I'm ready to work!"})
+
+#--------------ADD TO MYSQL-------------
+@server.route('/sql', methods=['POST'])
+def addSql():
+    email = request.json['email']
+    name = request.json['name']
+    password = hashlib.md5(request.json['password'].encode()).hexdigest()
+    curs = mysql.connection.cursor()
+    curs.execute("INSERT INTO Person (name, email, password) VALUES (%s, %s, %s);", (name, email, password))
+    mysql.connection.commit()
+    curs.close
+    return jsonify({'message': 'User successfully created.'})
 
 if __name__ == '__main__':
     server.run(debug=True, host='0.0.0.0', port=2800)
